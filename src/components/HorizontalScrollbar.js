a802bb2ef76e7ff5dc26a7fc7e6e32f1d4cf2152
apps221 <maggie.a.hong@gmail.com>
2025-02-28 18:52:31 -0500

    fixed category data

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -32,7 +32,7 @@ const HorizontalScrollbar = ({data, bodyPart, setBodyPart}) => {
  {data.map((item) => (
  <Box
 key={item.id || item}
-itemId={item.id || item}
+itemID={item.id || item}
 title={item.id || item}
 m="0 40px">
  <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
